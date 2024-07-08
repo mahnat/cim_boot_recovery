175ac47a5b0b3aa08929dc7d0de0db0c88c1184b
Marek Hnat <marek.hnat@gmail.com>
2024-07-08 17:25:21 +0000

    Remove BLynk and add IL9341 TFT associated libs and code change

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
+#include "stdio.h"
 #include "mbed.h"
-#include "LCD_DISCO_F746NG.h"
-#include "stm32746g_discovery_lcd.h"
+#include "SPI_TFT_ILI9341.h"
+#include "string"
+#include "Arial12x12.h"
+#include "Arial24x23.h"
+#include "Arial28x28.h"
+#include "font_big.h"
 
-LCD_DISCO_F746NG lcd;
+// the TFT connection to SPI
+SPI_TFT_ILI9341 TFT(p5, p6, p7, p8, p9, p10,"TFT"); // mosi, miso, sclk, cs, reset, dc
 
-DigitalIn uButton(USER_BUTTON);
+DigitalIn uButton(p21);
 DigitalOut led1(LED1);
 char buffer[50];
 
-DigitalInOut cimReset(D10);
-DigitalIn cimStatus(D9, PullUp);
+DigitalInOut cimReset(p22);
+DigitalIn cimStatus(p23, PullUp);
 
-DigitalOut cimCLK(D13);
-DigitalIn cimRX(D12, PullDown);
-DigitalOut cimTX(D11);
+DigitalOut cimCLK(p24);
+DigitalIn cimRX(p28, PullDown);
+DigitalOut cimTX(p27);
 
 uint8_t ArrECU[1024];
 
-Serial pc(SERIAL_TX, SERIAL_RX, 115200);
+Serial pc(USBTX, USBRX);
 
-void printBuf(uint8_t *buf, uint32_t len)
-{
+void printBuf(uint8_t *buf, uint32_t len) {
     pc.printf("Len %u\r\n", (uint16_t)len);
     for (uint32_t i = 0; i < len; i++)
     {
@@ -29,8 +34,7 @@ void printBuf(uint8_t *buf, uint32_t len)
     pc.printf("\r\n");
 }
 
-void slightDelay()
-{
+void slightDelay() {
     for (volatile uint32_t i = 0; i < 400; i++)
     {
         asm volatile("nop");
@@ -104,8 +108,7 @@ uint8_t shiftBits(uint8_t bits) {
 	return outval;
 }
 
-uint32_t sendCommand_len(uint8_t *cmd, uint8_t len, uint8_t *retBuf, uint32_t expLen)
-{
+uint32_t sendCommand_len(uint8_t *cmd, uint8_t len, uint8_t *retBuf, uint32_t expLen) {
     uint32_t retLen = 0;
 
     while(len--)
@@ -130,33 +133,43 @@ uint32_t sendCommand_len(uint8_t *cmd, uint8_t len, uint8_t *retBuf, uint32_t ex
     return retLen;
 }
 
-void printStatus(uint8_t statusbyte)
-{
+void printStatus(uint8_t statusbyte) {
 	statusbyte = (statusbyte >> 2)&3;
-    lcd.ClearStringLine(7);
+    //lcd.ClearStringLine(7);
+    TFT.locate(0,(7*12));
+    TFT.printf("                                  C");
 	switch(statusbyte) {
         case 0: 
             pc.printf("Key status: Not verified\r\n");
-            lcd.DisplayStringAtLine(7, (uint8_t *)"Key status: Not verified");
+            //lcd.DisplayStringAtLine(7, (uint8_t *)"Key status: Not verified");
+            TFT.locate(0,(7*12));
+            TFT.printf("Key status: Not verified");
             break;
         case 1: 
             pc.printf("Key status: Verification failed\r\n");
-            lcd.DisplayStringAtLine(7, (uint8_t *)"Key status: Verification failed");
+            //lcd.DisplayStringAtLine(7, (uint8_t *)"Key status: Verification failed");
+            TFT.locate(0,(7*12));
+            TFT.printf("Key status: Verification failed");
             break;
         case 2: 
             pc.printf("Key status: Unknown status 2\r\n");
-            lcd.DisplayStringAtLine(7, (uint8_t *)"Key status: Unknown status 2");
+            //lcd.DisplayStringAtLine(7, (uint8_t *)"Key status: Unknown status 2");
+            TFT.locate(0,(7*12));
+            TFT.printf("Key status: Unknown status 2");
             break;
         case 3: 
             pc.printf("Key status: Verified\r\n");
-            lcd.DisplayStringAtLine(7, (uint8_t *)"Key status: Verified");
+            //lcd.DisplayStringAtLine(7, (uint8_t *)"Key status: Verified");
+            TFT.locate(0,(7*12));
+            TFT.printf("Key status: Verified");
             break;
 	}
 }
 
-void printVersion()
-{
-    lcd.DisplayStringAtLine(4, (uint8_t *)"Version");
+void printVersion() {
+    //lcd.DisplayStringAtLine(4, (uint8_t *)"Version");
+    TFT.locate(0,(4*12));
+    TFT.printf("Version");
 
     uint8_t sendVer[2] = {0xFB, 0 };
     // 56 45 52 2e 34 2e 30 31
@@ -166,11 +179,12 @@ void printVersion()
     printBuf(ArrECU, 8);
     pc.printf("%s\r\n", ArrECU);
     
-    lcd.DisplayStringAtLine(5, (uint8_t *)ArrECU);
+    //lcd.DisplayStringAtLine(5, (uint8_t *)ArrECU);
+    TFT.locate(0,(5*12));
+    //TFT.printf((uint8_t *)ArrECU);
 }
 
-void printStatusRegs()
-{
+void printStatusRegs() {
 	uint8_t sendStatus[] = { 0x70, 0x00 };
 
     // Read status register
@@ -182,11 +196,14 @@ void printStatusRegs()
     printStatus(ArrECU[1]);
 }
 
-void cimPowerCycle(int seconds)
-{
+void cimPowerCycle(int seconds) {
     
-    lcd.ClearStringLine(11);
-    lcd.DisplayStringAtLine(11, (uint8_t *)"Toggle CIM power DOWN");
+    //lcd.ClearStringLine(11);
+    TFT.locate(0,(11*12));
+    TFT.printf("                                  C");
+    //lcd.DisplayStringAtLine(11, (uint8_t *)"Toggle CIM power DOWN");
+    TFT.locate(0,(11*12));
+    TFT.printf("Toggle CIM power DOWN");
     pc.printf("Toggle CIM power down\r\n");
     while(!cimStatus);
 
@@ -196,13 +213,21 @@ void cimPowerCycle(int seconds)
     cimCLK=0;
     cimStatus.mode(PullDown);
 
-    lcd.ClearStringLine(5);
-    lcd.ClearStringLine(7);
+    //lcd.ClearStringLine(5);
+    TFT.locate(0,(5*12));
+    TFT.printf("                                  C");
+    //lcd.ClearStringLine(7);
+    TFT.locate(0,(7*12));
+    TFT.printf("                                  C");
 
     for(int timeout = seconds + 1; timeout > 0; timeout--) {
-        lcd.ClearStringLine(11);
+        //lcd.ClearStringLine(11);
+        TFT.locate(0,(11*12));
+        TFT.printf("                                  C");
         sprintf(buffer, "Security wait timeout... %d seconds", timeout-1);
-        lcd.DisplayStringAtLine(11, (uint8_t *)buffer);
+        //lcd.DisplayStringAtLine(11, (uint8_t *)buffer);
+        TFT.locate(0,(11*12));
+        TFT.printf(buffer);
         pc.printf("Wait!\r\n");
         wait(1);
     }
@@ -212,12 +237,18 @@ void cimPowerCycle(int seconds)
     cimReset.input();
     cimStatus.mode(PullUp);
     
-    lcd.ClearStringLine(11);
-    lcd.DisplayStringAtLine(11, (uint8_t *)"Toggle CIM power UP");
+    //lcd.ClearStringLine(11);
+    TFT.locate(0,(11*12));
+    TFT.printf("                                  C");
+    //lcd.DisplayStringAtLine(11, (uint8_t *)"Toggle CIM power UP");
+    TFT.locate(0,(11*12));
+    TFT.printf("Toggle CIM power UP");
     pc.printf("Toggle CIM power up\r\n");
     while(cimStatus);
     
-    lcd.ClearStringLine(11);
+    //lcd.ClearStringLine(11);
+    TFT.locate(0,(11*12));
+    TFT.printf("                                  C");
 
 	resetTargetLong();
 
@@ -240,12 +271,16 @@ void crackKey(uint8_t *cmd, uint8_t len)
 	for (uint32_t i = 0; i < len; i++)
 		keyPtr[i] = 0x00;
 
-    lcd.ClearStringLine(8);
+    //lcd.ClearStringLine(8);
+    TFT.locate(0,(8*12));
+    TFT.printf("                                  C");
 	while (currKey < len)
 	{
         sprintf(buffer, "Key: CRACKING %d/%d [%02x:%02x:%02x:%02x:%02x:%02x:%02x]", currKey, len, 
             keyPtr[0], keyPtr[1], keyPtr[2], keyPtr[3], keyPtr[4], keyPtr[5], keyPtr[6]);  
-        lcd.DisplayStringAtLine(8, (uint8_t *)buffer);
+        //lcd.DisplayStringAtLine(8, (uint8_t *)buffer);
+        TFT.locate(0,(8*12));
+        TFT.printf(buffer);
         
 		// resetTarget();
 		asm volatile("cpsid if");
@@ -299,10 +334,14 @@ void crackKey(uint8_t *cmd, uint8_t len)
 		}
 	};
 
-    lcd.ClearStringLine(8);
+    //lcd.ClearStringLine(8);
+    TFT.locate(0,(8*12));
+    TFT.printf("                                  C");
     sprintf(buffer, "Key: %02x:%02x:%02x:%02x:%02x:%02x:%02x",
         keyPtr[0], keyPtr[1], keyPtr[2], keyPtr[3], keyPtr[4], keyPtr[5], keyPtr[6]);  
-    lcd.DisplayStringAtLine(8, (uint8_t *)buffer);
+    //lcd.DisplayStringAtLine(8, (uint8_t *)buffer);
+    TFT.locate(0,(8*12));
+    TFT.printf(buffer);
 
 	resetTargetLong();
 	wait_us(500);
@@ -318,7 +357,10 @@ void crackKey(uint8_t *cmd, uint8_t len)
     do {
         cimPowerCycle(5);
         
-        lcd.DisplayStringAtLine(11, (uint8_t *)"Checking...");
+        //lcd.DisplayStringAtLine(11, (uint8_t *)"Checking...");
+        TFT.locate(0,(11*12));
+        TFT.printf("Checking...");
+
         printBuf(cmd, 5 + len);
 
         sendCommand_len(cmd, 5 + len, 0, 0);
@@ -328,24 +370,28 @@ void crackKey(uint8_t *cmd, uint8_t len)
         sendCommand_len(sendStatus,  1, ArrECU, 2);
         printStatus(ArrECU[1]);
     
-        lcd.ClearStringLine(11);
+        //lcd.ClearStringLine(11);
+        TFT.locate(0,(11*12));
+        TFT.printf("                                  C");
     } while((ArrECU[1] & 0x0c) != 12 || ArrECU[0] != 0x80);
     
     printBuf(ArrECU, 2);
 
-    lcd.DisplayStringAtLine(11, (uint8_t *)"Checking... DONE");
+    //lcd.DisplayStringAtLine(11, (uint8_t *)"Checking... DONE");
+    TFT.locate(0,(11*12));
+    TFT.printf("Checking... DONE");
     pc.printf("Done\r\n");
     while(!uButton);
     while(uButton);
 }
 
 
-int main()
-{
+int main() {
     uint8_t sendKey[] = {
             0xF5, 0xDF, 0xff, 0x0f, 0x07,
             0x8b, 0x8e, 0x17, 0x3b, 0x2f, 0xec, 0xb8
     };
+    
 
     pc.printf("RESET\r\n");
     cimReset.output();
@@ -353,37 +399,52 @@ int main()
 
     // Enable DWT Timer
     CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
-    DWT->LAR = 0xC5ACCE55; 
+    ///DWT->LAR = 0xC5ACCE55; 
     DWT->CYCCNT = 0;
     DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
 
     led1 = 1;
 
-    lcd.SetFont(&Font16);
-    lcd.Clear(LCD_COLOR_DARKGRAY);
-    lcd.SetBackColor(LCD_COLOR_DARKGRAY);
-    lcd.SetTextColor(LCD_COLOR_WHITE);
+    //TFT.claim(stdout);      // send stdout to the TFT display
+    //TFT.claim(stderr);      // send stderr to the TFT display
+    TFT.set_orientation(3);
+    TFT.background(White);    // set background to white
+    TFT.foreground(Black);    // set chars to white
+    TFT.set_font((unsigned char*) Arial12x12);
+    TFT.cls();
 
-    lcd.DisplayStringAtLine(0, (uint8_t *)"SAAB CIM bootloader key cracker");
+    //lcd.DisplayStringAtLine(0, (uint8_t *)"SAAB CIM bootloader key cracker");
+    TFT.locate(0,0);
+    TFT.printf("SAAB CIM bootloader key cracker");
 
     sprintf(buffer, "CPU is %d Hz", SystemCoreClock);  
-    lcd.DisplayStringAtLine(1, (uint8_t *)buffer);
+    //lcd.DisplayStringAtLine(1, (uint8_t *)buffer);
+    TFT.locate(0,(1*12));
+    TFT.printf(buffer);
 
     resetTarget();
 
     wait(2);
     
-    lcd.DisplayStringAtLine(11, (uint8_t *)"Turn CIM power UP...");
+    //lcd.DisplayStringAtLine(11, (uint8_t *)"Turn CIM power UP...");
+    TFT.locate(0,(11*12));
+    TFT.printf("Turn CIM power UP...");
     while(cimStatus.read());
 
-    lcd.ClearStringLine(11);
-    lcd.DisplayStringAtLine(2, (uint8_t *)"Starting...");
+    //lcd.ClearStringLine(11);
+    TFT.locate(0,(11*12));
+    TFT.printf("                                  C");
+    //lcd.DisplayStringAtLine(2, (uint8_t *)"Starting...");
+    TFT.locate(0,(2*12));
+    TFT.printf("Starting...");
   
     while(1)
     {
         led1=0;
         for(unsigned int line = 4; line < 20; line++) {
-            lcd.ClearStringLine(line);
+            //lcd.ClearStringLine(line);
+            TFT.locate(0,(line*12));
+            TFT.printf("                                  C");
         }
     
         resetTarget();
